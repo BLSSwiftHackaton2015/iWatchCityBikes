9b1f7fac511553e72b77bafe6d84fa80d83cb09b
user <user@users-MacBook-2.local>
2015-06-13 13:32:31 +0200

    mamy jsona sparsowanego

--- iWatchCityBikes/iWatchCityBikes/NetworkManager.swift
+++ iWatchCityBikes/iWatchCityBikes/NetworkManager.swift
@@ -7,21 +7,32 @@
 //
 
 import UIKit
+import Foundation
 
 class NetworkManager: NSObject {
     class func getData() {
         let baseURL =  NSURL(string: "https://www.bikes-srm.pl/LocationsMap.aspx")
         let task = NSURLSession.sharedSession().dataTaskWithURL(baseURL!) {(data, response, error) in
-            println(NSString(data: data, encoding: NSUTF8StringEncoding))
+            //println(NSString(data: data, encoding: NSUTF8StringEncoding))
+            self.parse(NSString(data: data, encoding: NSUTF8StringEncoding)!)
         }
         
         task.resume()
     }
     
-    private func parse(NSString inputPage){
-//        var str = inputPage
-//        let pos = str.rangeOfString("var mapDataLocations = ")
-//        jsonStr = inputPage.
+    private class func parse(inputPage: NSString){
+        //var str = inputPage
+        //let pos = str.rangeOfString()
+        let jsonStr1:NSString = inputPage.componentsSeparatedByString("var mapDataLocations = ")[1] as! NSString
+        let jsonStr2:NSString = jsonStr1.componentsSeparatedByString(";")[0] as! NSString
+        println(jsonStr2)
         
+        var jsonError:NSError?
+        let jsonData = jsonStr2.dataUsingEncoding(NSUTF8StringEncoding);
+        let json = NSJSONSerialization.JSONObjectWithData(jsonData!, options: nil, error: &jsonError) as! NSArray
+        
+        if let unwrappedError = jsonError {
+            println(unwrappedError)
+        }
     }
 }
